ad350df5534d05d6cda86a2c4172e70f83e19e84
Edward Weymouth <97198885+edwardweymouth-wk@users.noreply.github.com>
2024-06-25 16:20:44 -0600

    all the things

---
+++ src/App.tsx
@@ -0,0 +1,272 @@
+
+import workivaLogo from "./assets/workiva.svg";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div
+          style={{
+            background: "#fff",
+            borderBottom: "1px solid rgba(0, 0, 0, 0.16)",
+            display: "flex",
+            paddingTop: "5px",
+            paddingBottom: "5px",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ display: "flex", width: "200px" }}>
+            <div style={{ marginLeft: "7px", width: "54px" }}>
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "34px",
+                  height: "34px",
+                  borderRadius: "50%",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                }}
+              ></div>
+            </div>
+            <div
+              style={{
+                backgroundImage: `url(${workivaLogo})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "left center",
+                backgroundColor: "rgba(0, 0, 0, 0)",
+                width: "100%",
+                height: "100%",
+                backgroundSize: "auto 21px",
+              }}
+            ></div>
+          </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div
+              style={{
+                display: "block",
+                backgroundColor: "rgba(0, 0, 0, 0.11)",
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+              }}
+            ></div>
+            <div
+              style={{
+                display: "block",
+                backgroundColor: "rgba(0, 0, 0, 0.11)",
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                marginLeft: "20px",
+              }}
+            ></div>
+            <div
+              style={{
+                display: "block",
+                backgroundColor: "rgba(0, 0, 0, 0.11)",
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                marginLeft: "20px",
+              }}
+            ></div>
+            <div
+              style={{
+                display: "block",
+                backgroundColor: "rgba(0, 0, 0, 0.11)",
+                width: "160px",
+                height: "34px",
+                borderRadius: "4px",
+                animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                marginLeft: "20px",
+                marginRight: "5px",
+              }}
+            ></div>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            height: "99vh",
+            backgroundColor: "#fff",
+            boxSizing: "border-box",
+          }}
+        >
+          <div
+            style={{
+              borderRight: "1px solid rgba(0, 0, 0, 0.16)",
+              width: "200px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div
+              style={{
+                borderBottom: "1px solid rgba(0, 0, 0, 0.16)",
+                height: "60px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "5px",
+                }}
+              ></div>
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "70%",
+                  height: "20px",
+                  borderRadius: "4px",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "15px",
+                }}
+              ></div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+
+                height: "44px",
+              }}
+            >
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "5px",
+                }}
+              ></div>
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "70%",
+                  height: "20px",
+                  borderRadius: "4px",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "15px",
+                }}
+              ></div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "44px",
+              }}
+            >
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "5px",
+                }}
+              ></div>
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "70%",
+                  height: "20px",
+                  borderRadius: "4px",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "15px",
+                }}
+              ></div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "44px",
+              }}
+            >
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "5px",
+                }}
+              ></div>
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "70%",
+                  height: "20px",
+                  borderRadius: "4px",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "15px",
+                }}
+              ></div>
+            </div>
+          </div>
+          <div 
+          style ={{
+            backgroundColor: '#fff',
+            width: '80%'
+          }}
+          >
+          <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "160px",
+                  height: "24px",
+                  borderRadius: "4px",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "20px",
+                  marginTop: "20px"
+                }}
+              ></div>
+              <div
+                style={{
+                  display: "block",
+                  backgroundColor: "rgba(0, 0, 0, 0.11)",
+                  width: "320px",
+                  height: "12px",
+                  borderRadius: "4px",
+                  animation: "animation-pulse 2s ease-in-out 0.5s infinite",
+                  marginLeft: "20px",
+                  marginTop: "10px"
+                }}
+              ></div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
